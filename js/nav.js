3fe15c82987dc21ff5289b9a02ec8544c3af218f
xbljy <2436888138@qq.com>
2023-09-07 07:42:58 +0800

    first commit

---
+++ js/nav.js
@@ -0,0 +1,37 @@
+$(function(){
+		$('.navi li').hover(function() {
+			$(this).find('.two-nav').stop().slideDown()
+		}, function() {
+			$(this).find('.two-nav').stop().slideUp()
+		});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
